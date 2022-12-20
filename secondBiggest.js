d6d6921e2cb8b3e0c15081581c3a4b3423aff3a8
YoadBarok <74326426+YoadBarok@users.noreply.github.com>
2022-12-20 11:36:02 +0100

    First commit

---
+++ secondBiggest.js
@@ -0,0 +1,25 @@
+function getSecondBiggest(arr) {
+
+    if (!Array.isArray(arr)) {
+        return "Invalid input!";
+    }
+
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    let biggest = -Infinity;
+    let secondBiggest;
+
+    arr.forEach(item => {
+        if (item > biggest) {
+            secondBiggest = biggest;
+            biggest = item;
+        } else if (item > secondBiggest) {
+            secondBiggest = item;
+        }
+    });
+    return secondBiggest;
+}
+
+module.exports = getSecondBiggest;
---
+++ secondBiggest.test.js
@@ -0,0 +1,40 @@
+const getSecondBiggest = require('./secondBiggest');
+
+
+describe("Testing on fixed values", () => {
+    test("Should return 8", () => {
+        const arr = [5, 2, 3, 1, 8, 10, 6];
+        expect(getSecondBiggest(arr)).toBe(8);
+    });
+    test("Should return 5", () => {
+        const arr = [4, 6, 2, 5, 3];
+        expect(getSecondBiggest(arr)).toBe(5);
+    });
+    test("Should return -10", () => {
+        const arr = [-100, -24, -10, 1];
+        expect(getSecondBiggest(arr)).toBe(-10);
+    });
+    test("Should return `Invalid input!`", () => {
+        const arr = ".";
+        expect(getSecondBiggest(arr)).toBe("Invalid input!");
+    });
+    test("Should return 0", () => {
+        const arr = [];
+        expect(getSecondBiggest(arr)).toBe(0);
+    })
+
+})
+
+
+describe("Testing with random values", () => {
+    test("Random test 1", () => {
+        const arr = Array.from({length: 40}, () => Math.floor(Math.random() * 40));
+        arr.sort((a, b) => b - a);
+        expect(getSecondBiggest(arr)).toBe(arr[1]);
+    });
+    test("Random test 2", () => {
+        const arr = Array.from({length: 40}, () => Math.floor(Math.random() * 40));
+        arr.sort((a, b) => b - a);
+        expect(getSecondBiggest(arr)).toBe(arr[1]);
+    });
+})
